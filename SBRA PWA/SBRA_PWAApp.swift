91f1cc4baa30779adb86f7c4ab12a847068aa426
ProgerUser <said.pachuliya@mail.ru>
2026-02-17 23:34:34 +0300

    Initial Commit

---
+++ SBRA PWA/SBRA_PWAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBRA_PWAApp.swift
+//  SBRA PWA
+//
+//  Created by Саид Пачулия on 17.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SBRA_PWAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
